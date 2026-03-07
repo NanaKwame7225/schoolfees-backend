670b42cabb5dba66f57f513a5bb7c4883c0fbaa9
Nana Kwame Asomani-Appah <Nanakwameasomani2@gmail.com>
2026-03-07 01:31:41 +0000

    auto seed database

--- src/server.js
+++ src/server.js
@@ -50,6 +50,9 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
+
+    require('./seed');   // 👈 ADD THIS LINE HERE
+
     app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch(err => {
